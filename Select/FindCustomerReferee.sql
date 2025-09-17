c6a2b804f8950d4b7fc6a99753db615256876028
codeVibes:) <123478862+vanshika241@users.noreply.github.com>
2025-09-18 00:12:36 +0530

    Update FindCustomerReferee.sql

--- Select/FindCustomerReferee.sql
+++ Select/FindCustomerReferee.sql
@@ -1,2 +1,6 @@
 -- Write your MySQL query statement below
-SELECT name FROM Customer WHERE referee_id  != 2 OR referee_id  IS NULL;
+-- names - Customer 
+-- referred id != 2 or referred id == null - either means or 
+-- referred id - Customer
+
+SELECT name from Customer WHERE referee_id != 2 OR referee_id IS NULL;
