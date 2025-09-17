df1c6d2f68a260198e6230c9128a1bb9d0addac2
codeVibes:) <123478862+vanshika241@users.noreply.github.com>
2025-09-18 00:20:21 +0530

    Update BigCountries.sql

--- Select/BigCountries.sql
+++ Select/BigCountries.sql
@@ -1,2 +1,5 @@
 -- Write your MySQL query statement below
-SELECT name, population, area from World WHERE area >= 3000000 OR population >= 25000000;
+-- World - name , population , area 
+-- condition - population (World) > 25000000 || area (World) > 3000000 
+
+SELECT name , population , area from World WHERE population >= 25000000 OR area >= 3000000;
