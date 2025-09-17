1740135e0feac67f45b47d3b85f0389f59f94436
codeVibes:) <123478862+vanshika241@users.noreply.github.com>
2025-09-18 00:07:40 +0530

    Update RecyclableAndLowFatProducts.sql

--- Select/RecyclableAndLowFatProducts.sql
+++ Select/RecyclableAndLowFatProducts.sql
@@ -1,2 +1,4 @@
 -- Write your MySQL query statement below
-SELECT product_id FROM Products WHERE low_fats = 'Y' AND recyclable = 'Y';
+--  result - id - > profuct table 
+--  condition - fat = y and recycalable = 'Y'
+SELECT product_id from Products WHERE low_fats = 'Y' AND recyclable  = 'Y';
