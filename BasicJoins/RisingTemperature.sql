de24419539cc48e1d9f8f4dde986c6ab47599a67
codeVibes:) <123478862+vanshika241@users.noreply.github.com>
2025-09-19 00:30:07 +0530

    Create RisingTemperature.sql

---
+++ BasicJoins/RisingTemperature.sql
@@ -0,0 +1,9 @@
+--# Write your MySQL query statement below
+
+-- DATEDIFF(date1 , date2 ) =1 yesterday 
+-- SELF JOIN - W1.data is one day next to W2.date 
+SELECT w1.id 
+FROM Weather w1 
+INNER JOIN Weather w2 
+ON DATEDIFF(w1.recordDate , w2.recordDate )=1 AND 
+w1.temperature > w2.temperature;
