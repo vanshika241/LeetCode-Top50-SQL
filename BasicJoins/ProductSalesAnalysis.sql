b9b480fec03c999c8f760a7b73f16e1a069a2463
codeVibes:) <123478862+vanshika241@users.noreply.github.com>
2025-09-18 23:55:46 +0530

    Update ProductSalesAnalysis.sql

--- BasicJoins/ProductSalesAnalysis.sql
+++ BasicJoins/ProductSalesAnalysis.sql
@@ -1,4 +1,9 @@
 -- Write your MySQL query statement below
-SELECT pn.product_name , p.year , p.price
-FROM Product pn 
-RIGHT JOIN Sales p ON pn.product_id = p.product_id;
+-- Return - product_name (Product), year (Sales), and price(Sales)
+-- Common - product_id 
+-- All values of Sales  - Left Join 
+
+SELECT p.product_name , s.year, s.price 
+FROM Sales s 
+Left JOIN Product p 
+ON s.product_id = p.product_id ;
